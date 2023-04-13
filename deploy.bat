5b6c9bb9389948e5269e9b56d6f8c85cdd65d666
mwangli <1325533186@qq.com>
2023-04-13 11:05:21 +0800

    配置修改

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,6 @@
 set HOST=root@test
 set APP=found-front
 scp -r Dockerfile %HOST%:build
-scp -r dist/* %HOST%:build/
+ssh %HOST% rm -rf build/dist
+scp -r dist %HOST%:build
 ssh %HOST% build/build.sh %APP%
