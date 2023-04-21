fe592f03d8078e1e3f62e95e2f9ef058b8b6fefe
mwangli <1325533186@qq.com>
2023-04-21 20:28:38 +0800

    v2.3

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -21,7 +21,7 @@ const Footer: React.FC = () => {
       links={[
         {
           key: 'Found-Trading System',
-          title: 'Found-Trading System v2.2.2',
+          title: 'Found-Trading System v2.3',
           href: 'https://github.com/mwangli',
           blankTarget: true,
         },
