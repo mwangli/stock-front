8a3ba7b64bb8e089447169e8eb6e71f0674e0f9a
mwangli <1325533186@qq.com>
2024-03-28 21:59:40 +0800

    aa

--- src/access.ts
+++ src/access.ts
@@ -2,10 +2,9 @@
  * @see https://umijs.org/zh-CN/plugins/plugin-access
  * */
 export default function access(initialState: { currentUser?: API.CurrentUser } | undefined) {
-  const { currentUser } = initialState ?? {};
+  const {currentUser} = initialState ?? {};
   return {
-    canAdmin: currentUser && currentUser.access === 'admin',
+    canAdmin: currentUser && (currentUser.access === 'admin' || currentUser.access === 'guest'),
     canDemo: currentUser && currentUser.access === 'demo',
-    canGuest: currentUser && currentUser.access === 'guest',
   };
 }
