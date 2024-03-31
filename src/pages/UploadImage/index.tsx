79cbd428bcf8a53f3319b8683e34ab8901869740
mwangli <1325533186@qq.com>
2024-03-31 21:06:12 +0800

    文件下载优化

--- src/pages/UploadImage/index.tsx
+++ src/pages/UploadImage/index.tsx
@@ -383,42 +383,53 @@ const TableList: React.FC = () => {
      */
     async function downloadFile(url: string, filename: string = 'test.xlsx') {
       let blob = await request(url, {responseType: 'blob'}); // 设置responseType响应类型为blob,响应的直接是个blob对象
-
+      handleExport(blob)
     }
 
-    function getNowDate() {
-      const now = new Date();
-      const year = now.getFullYear();
-      const month = ('0' + (now.getMonth() + 1)).slice(-2);
-      const day = ('0' + now.getDate()).slice(-2);
-      const hours = ('0' + now.getHours()).slice(-2);
-      const minutes = ('0' + now.getMinutes()).slice(-2);
-      const seconds = ('0' + now.getSeconds()).slice(-2);
-      return year + month + day + hours + minutes + seconds;
+
+  function getNowDate() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = ('0' + (now.getMonth() + 1)).slice(-2);
+    const day = ('0' + now.getDate()).slice(-2);
+    const hours = ('0' + now.getHours()).slice(-2);
+    const minutes = ('0' + now.getMinutes()).slice(-2);
+    const seconds = ('0' + now.getSeconds()).slice(-2);
+    return year + month + day + hours + minutes + seconds;
+  }
+
+    /* 下载文件的公共方法，参数就传blob文件流*/
+   function  handleExport(data:any) {
+      // 动态创建iframe下载文件
+      let fileName = "test-" + getNowDate() + ".xlsx";
+      if (!data) {
+        return;
+      }
+      let blob = new Blob([data], { type: "application/octet-stream" });
+      if ("download" in document.createElement("a")) {
+        // 不是IE浏览器
+        let url = window.URL.createObjectURL(blob);
+        let link = document.createElement("a");
+        link.style.display = "none";
+        link.href = url;
+        link.setAttribute("download", fileName);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link); // 下载完成移除元素
+        window.URL.revokeObjectURL(url); // 释放掉blob对象
+      } else {
+        // IE 10+
+        window.navigator.msSaveBlob(blob, fileName);
+      }
     }
 
 
     return (
       <PageContainer>
         <UploadForm></UploadForm>
-        {/*<Flex gap="small" wrap="wrap">*/}
         <Button type="primary" icon={<DownloadOutlined/>} size={"large"}
                 onClick={() => {
-                  download({}).then((blob) => {
-                    let reader = new FileReader(); // 创建一个file对象
-                    // @ts-ignore
-                    reader.readAsDataURL(blob);  // 转换为base64，可以直接放入a标签的href
-                    reader.onload = function (e: any) {
-                      // 转换完成，创建一个a标签用于下载
-                      let a = document.createElement('a');
-                      a.download = "test-" + getNowDate() + ".xlsx";
-                      a.href = e.target.result;
-                      // 在body中插入a元素
-                      document.body.insertAdjacentElement('afterend', a);
-                      a.click();
-                      a.remove();
-                    }
-                  })
+                  download({}).then((blob) => handleExport(blob))
                 }}
         >
           Download
