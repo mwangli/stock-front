7f63115f191ce50a66e6712538a765c4905cb2ab
mwangli <1325533186@qq.com>
2024-04-14 21:49:59 +0800

    增加项目文档链接

--- src/app.tsx
+++ src/app.tsx
@@ -144,8 +144,9 @@ export const layout: RunTimeLayoutConfig = ({initialState, setInitialState}) => 
         width: '331px',
       },
     ],
-    links: isDev
-      ? [
+    // links: isDev ?
+   links :
+      [
         <Link
           key="openapi"
               to="/"
@@ -157,8 +158,8 @@ export const layout: RunTimeLayoutConfig = ({initialState, setInitialState}) => 
            <span>项目文档</span>
         </a>
         </Link>,
-      ]
-      : [],
+      ],
+      // : [],
     menuHeaderRender: undefined,
     // 自定义 403 页面
     // unAccessible: <div>unAccessible</div>,
